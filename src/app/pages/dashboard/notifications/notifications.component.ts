a4fe08a687bb6de7dc6fc0b6692c598eb68c27ba
Mohamed Gamal <jaiden90@gmail.com>
2024-03-10 17:56:01 +0200

    Minor enhancements

--- src/app/pages/dashboard/notifications/notifications.component.ts
+++ src/app/pages/dashboard/notifications/notifications.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.css'
 })
 export class NotificationsComponent {
-
+  notifications = [
+    {title: 'Profile data has been updated', createdAt: '03-04-2024'},
+    {title: 'Profile data has been updated', createdAt: '03-03-2024'},
+    {title: 'Account Has been verified', createdAt: '03-03-2024'},
+  ];
 }
